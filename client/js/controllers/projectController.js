51e1a2d1ed81f98928f1aa13168e3e1b14a41d56
Webdown2 <que.babault@gmail.com>
2015-11-03 18:48:27 +0100

    adding a table to manipulate customers

--- client/js/controllers/projectController.js
+++ client/js/controllers/projectController.js
@@ -1,8 +1,22 @@
 myApp.controller('projectController', function ($scope, Api) {
     $scope.form = {};
+    $scope.customers = [];
+
+    Api.Customer.query({}, function (data) {
+        $scope.customers = data;
+    });
+
+    $scope.delete = function(customer) {
+        Api.Customer.delete({id: customer._id}, function (data) {
+            var index = $scope.customers.indexOf(customer);
+            $scope.customers.splice(index, 1);
+        });
+    };
+
     $scope.addToDatabase = function () {
         Api.Customer.save({}, $scope.form, function () {
+            $scope.customers.push($scope.form);
             $scope.form = {};
         })
     }
-})
+});
